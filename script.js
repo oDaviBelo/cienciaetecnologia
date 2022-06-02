35b72f088d6e4aad19836d80ee8e42928bc58a38
Davi Belo <davibelocontato@gmail.com>
2022-06-02 01:42:03 -0300

    terceiro

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function subirtela() {
-    window.scrollTo({
-        top: 0,
-        behavior:'smooth'
-    });
-}
-
-var verificar = setInterval(()=>{
-    if(window.scrollY == 0){
-        document.querySelector('.butao').style.opacity = 0;
-        document.querySelector('.butao').style.transition = '0.3s ease';
-    }
-    else{
-        document.querySelector('.butao').style.opacity = 1;
-    }
-}
-,10)
-
 
 function openmenu() {
     if(document.querySelector('#menu-area').style.width=="0px") {
